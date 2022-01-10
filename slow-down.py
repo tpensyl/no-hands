2e8f1fc5cb538e4ebc5c3b781589faeedcd164dd
Tommy Pensyl <tpensyl@bandwidth.com>
2022-01-10 13:54:43 -0500

    add script for yelling at fast typers

---
+++ slow-down.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import argparse
+import pynput
+import time
+
+DEFAULT_WPM = 40
+BUFFER_S = 1 # seconds
+
+####### PARSE
+parser = argparse.ArgumentParser(description='Yell at you when you type too fast.')
+parser.add_argument('--wpm', action='store', type=int, default=DEFAULT_WPM,
+                    help='Maximum typing speed in wpm')
+args = parser.parse_args()
+
+MAX_STROKES_PER_SECOND = args.wpm * 5 / 60
+MAX_TYPING_SLICE = 1. / MAX_STROKES_PER_SECOND
+
+####### RATE LIMITER
+global nextAllowedKeyStroke
+nextAllowedKeyStroke = time.time()
+def rateLimit(k):
+	global nextAllowedKeyStroke
+	t = time.time()
+	#print(t, nextAllowedKeyStroke)
+	if nextAllowedKeyStroke > t + BUFFER_S:
+		nextAllowedKeyStroke = t + BUFFER_S
+		print("\a")
+	else:
+		print(nextAllowedKeyStroke, MAX_TYPING_SLICE)
+		nextAllowedKeyStroke = max(nextAllowedKeyStroke, time.time()) + MAX_TYPING_SLICE
+
+def start():
+	keyStateListener = pynput.keyboard.Listener(
+	    on_press=rateLimit
+	    #on_release=rateLimit
+	)
+	keyStateListener.start()
+
+	while(True):
+		time.sleep(5) # long-living process
+
+start()
+
